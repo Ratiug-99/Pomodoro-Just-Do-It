e373f9a39f41b90314244aaf690658c459844c25
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-09-11 20:56:26 +0300

    start TimerService (without Timer). Add logD

--- app/src/main/java/com/ratiug/dev/pomodorojustdoit/MainActivity.java
+++ app/src/main/java/com/ratiug/dev/pomodorojustdoit/MainActivity.java
@@ -1,20 +1,49 @@
 package com.ratiug.dev.pomodorojustdoit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DBG | MainActivity | ";
+    //
+    ServiceConnection mServiceConn;
+    TimerService myTimerService;
+    Boolean bound = false;
+    Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIntent = new Intent(this,TimerService.class);
+
+        mServiceConn = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                myTimerService = ((TimerService.MyBinder) iBinder).getService();
+                bound = true;
+                Log.d(TAG, "onServiceConnected");
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+                bound = false;
+                Log.d(TAG, "onServiceDisconnected");
+            }
+        };
+        startTimer();
+        bindService(mIntent,mServiceConn,0);
+        startService(mIntent);
     }
 
-    private void startTimer(){
+    private void startTimer() {
         Log.d(TAG, "startTimer");
     }
 
--- app/src/main/java/com/ratiug/dev/pomodorojustdoit/TimerService.java
+++ app/src/main/java/com/ratiug/dev/pomodorojustdoit/TimerService.java
@@ -2,29 +2,49 @@ package com.ratiug.dev.pomodorojustdoit;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 public class TimerService extends Service {
+    MyBinder mBinder = new MyBinder();
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        super.onCreate();
+    }
+
     private static final String TAG = "DBG | TimerService | ";
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         Log.d(TAG, "onBind");
-        return null;
-    }
 
+        return mBinder;
+    }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
+        start();
         return super.onStartCommand(intent, flags, startId);
     }
-
     @Override
     public void onDestroy() {
         Log.d(TAG, "onDestroy");
         super.onDestroy();
+    }
+
+    void start(){
+        Log.d(TAG, "start: ");
+    }
+
+    class MyBinder extends Binder {
+        TimerService getService(){
+            Log.d(TAG, "getService");
+            return TimerService.this;
+        }
     }
 }
